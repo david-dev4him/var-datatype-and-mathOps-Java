4bb4d4d058e0185c52cf0332278b2c421e1c87a4
david-dev4him <davidonshipping@gmail.com>
2021-12-26 07:27:47 -0300

    variaveis-tipodedados-em-Java

---
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,43 @@
+package one.digitalinnovation.basecamp;
+import java.sql.SQLOutput
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i;
+        int I;
+        int _1a; // deve ser evitado, embora não apresente erro de compilacao //
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j;
+        j = 15;
+        int asrn24678nd;
+        int asrn2$678_nd = 10; //nao apresenta uma boa pratica a utilizacao de caracters especiais //
+
+        asrn24678nd = 100;
+
+        int quantidadeProduto  = 50;
+        final int NUMERO_TENTATIVAS = 5;
+        final int numero_tentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678nd);
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+    }
+}
